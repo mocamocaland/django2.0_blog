865b40162b7769cb60b51e816d2554cd5e579dc3
mocamocaland <noi_kaas@yahoo.co.jp>
2018-03-05 19:40:12 +0900

    送信フォームを作成

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import DayCreateForm
 
 
 def index(request):
     return render(request, 'blog/day_list.html')
+
+
+def add(request):
+    # 送信内容を基にフォームを作る。POSTでなければ空フォーム
+    form = DayCreateForm(request.POST or None)
+
+    # method=POST,送信ボタンを押した時、入力内容が問題なければ
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        return redirect('diary:index')
+
+    # 通常のアクセスページや、入力内容に誤りがあればまたぺーじを表示
+    context = {
+        'form': form
+    }
+    return render(request, 'blog/day_form.html', context)
