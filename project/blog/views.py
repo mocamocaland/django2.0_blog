aa0892005e3ab7da64882653a9f725e7c71bb4b7
mocamocaland <noi_kaas@yahoo.co.jp>
2018-03-05 20:02:39 +0900

    投稿内容が反映されるように昨日追加

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
 from .forms import DayCreateForm
+from .models import Day
 
 
 def index(request):
-    return render(request, 'blog/day_list.html')
+    context = {
+        'day_list': Day.objects.all(),
+    }
+    return render(request, 'blog/day_list.html', context)
 
 
 def add(request):
@@ -13,7 +17,7 @@ def add(request):
     # method=POST,送信ボタンを押した時、入力内容が問題なければ
     if request.method == 'POST' and form.is_valid():
         form.save()
-        return redirect('diary:index')
+        return redirect('blog:index')
 
     # 通常のアクセスページや、入力内容に誤りがあればまたぺーじを表示
     context = {
